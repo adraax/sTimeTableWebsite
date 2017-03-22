1f93e6a172fb3d621c8ac4272b2046f215b40687
Olivier MERLIN <merlin.olivier71@gmail.com>
2017-03-22 14:08:38 +0100

    change about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,1 @@
+Pour le about on peut mettre un truc du genre : Application à destination des étudiants de l'Université Paul Sabatier. Gérez votre emploi du temps depuis votre smartphone, ajoutez vos enseignants et notifiez-les d'un retard ou d'une absence. Ajoutez vos événements personnels et modifiez leur couleur à volonté.
--- resources/views/install.blade.php
+++ resources/views/install.blade.php
@@ -102,8 +102,12 @@
         <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
             <div class="sidebar-module sidebar-module-inset">
                 <h4>About</h4>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit
-                    amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                <p>
+                    Gérez votre emploi du temps depuis votre smartphone, ajoutez vos enseignants et notifiez-les d'un
+                    retard ou d'une absence.
+                    <br>
+                    Ajoutez vos événements personnels et modifiez leur couleur à volonté.
+                </p>
             </div>
             <div class="sidebar-module">
                 <h4>Archives</h4>
