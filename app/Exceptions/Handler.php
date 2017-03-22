06b0a73df4cc4a020a27f2bf68dd87f0ccdf7524
Olivier MERLIN <merlin.olivier71@gmail.com>
2017-03-22 02:38:13 +0100

    handle all errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,6 +44,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        return view('errors.generic');
         return parent::render($request, $exception);
     }
 
