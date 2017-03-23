788c9844afc0c4cb924ed8a65cd2f9d28655851b
Adraax <merlin.olivier71@gmail.com>
2017-03-23 15:42:30 +0100

    test refactor bug

--- database/seeds/BugsTableSeeder.php
+++ database/seeds/BugsTableSeeder.php
@@ -10,13 +10,20 @@ class BugsTableSeeder extends Seeder
     {
         DB::table('bugs')->delete();
 
+        $status = [
+            'open',
+            'investigation',
+            'confirmed',
+            'close'
+        ];
+
         for ($i = 0; $i < 250; ++$i) {
             $date = $this->randDate();
             DB::table('bugs')->insert([
                 'title' => 'Titre' . $i,
                 'body' => 'Contenu' . $i . ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                 'user_id' => rand(1, 50),
-                'open' => rand(0, 1),
+                'status' => $status[rand(0, 3)],
                 'created_at' => $date,
                 'updated_at' => $date
             ]);
--- resources/views/bug/index.blade.php
+++ resources/views/bug/index.blade.php
@@ -5,26 +5,37 @@
 @endsection
 
 @section('content')
-    <div class="col-m-offset-1 col-m-10">
-        <div class="row">
-            <div class="col-m-8">
-                <div class="pull-left">
-                    <span class="glyphicon glyphicon-remove"></span> {{$open}} Ouverts &nbsp; <span
-                            class="glyphicon glyphicon-ok"></span> {{$close}} fermés
+    <div class = "col-m-offset-1 col-m-10">
+        <div class = "row">
+            <div class = "col-m-8">
+                <div class = "pull-left">
+                    <span class = "glyphicon glyphicon-remove"></span> {{$open}} Ouverts &nbsp; <span
+                            class = "glyphicon glyphicon-ok"></span> {{$close}} résolus
                 </div>
-                <div class="pull-right">
-                    <button class="btn btn-default">
-                        <a href="{!! route('bug.create') !!}">Créer un rapport de bug</a>
+                <div class = "pull-right">
+                    <button class = "btn btn-default">
+                        <a href = "{!! route('bug.create') !!}">Créer un rapport de bug</a>
                     </button>
                 </div>
             </div>
         </div>
-        <ul class="list-group">
+        <ul class = "list-group">
             @forelse($bugs as $bug)
-                <li class="list-group-item"><a href="{!! route('bug.show', $bug->id) !!}">{{$bug->title}}</a> - par <a
-                            href="{!! route('user.show', $bug->user->id ) !!}">{{$bug->user->name}}</a></li>
+                @if($bugs->status == "open")
+                    <span class = "label label-danger">Ouvert</span>
+                @elseif($bugs->status == "investigation")
+                    <span class = "label label-warning">Investigation</span>
+                @elseif($bugs->status == "confirmed")
+                    <span class = "label label-info">Confirmé</span>
+                @elseif($bugs->status == "close")
+                    <span class = "label label-success">Résolu</span>
+                @endif
+                <li class = "list-group-item">
+                    {{$bug->title}}</a> - par <a
+                            href = "{!! route('user.show', $bug->user->id ) !!}">{{$bug->user->name}}</a>
+                </li>
             @empty
-                <li class="list-group-item">Pas (encore !) de bugs.</li>
+                <li class = "list-group-item">Pas (encore !) de bugs.</li>
             @endforelse
         </ul>
         {!! $links !!}
