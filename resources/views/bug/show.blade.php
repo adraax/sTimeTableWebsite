f6c494671993e054793d79707b94edb4915baa3b
Adraax <merlin.olivier71@gmail.com>
2017-03-23 16:05:36 +0100

    update views

--- resources/views/bug/show.blade.php
+++ resources/views/bug/show.blade.php
@@ -4,18 +4,29 @@
 @endsection
 
 @section('content')
-    <div class="col-m-offset-1 col-m-10">
+    <div class = "col-m-offset-1 col-m-10">
         <br>
-        <div class="panel panel-default">
-            <div class="panel-heading">{{$bug->title}}</div>
-            <div class="panel-body">
+        <div class = "panel panel-default">
+            <div class = "panel-heading">
+                @if($bug->status == "open")
+                    <span class = "label label-danger">Ouvert</span>
+                @elseif($bug->status == "investigation")
+                    <span class = "label label-warning">Investigation</span>
+                @elseif($bug->status == "confirmed")
+                    <span class = "label label-info">Confirmé</span>
+                @elseif($bug->status == "close")
+                    <span class = "label label-success">Résolu</span>
+                @endif
+                {{$bug->title}}
+            </div>
+            <div class = "panel-body">
                 {{$bug->body}}
             </div>
-            <div class="panel-footer">
-                <div class="pull-right">
+            <div class = "panel-footer">
+                <div class = "pull-right">
                     Le {{$bug->getDate()}} par {{$bug->user->name}}
                 </div>
-                <div class="clearfix"></div>
+                <div class = "clearfix"></div>
             </div>
         </div>
 
--- resources/views/comment/create.blade.php
+++ resources/views/comment/create.blade.php
@@ -1,5 +1,5 @@
 <div class = "panel panel-primary">
-    <div class = "panel-heading">Rapport de bug</div>
+    <div class = "panel-heading">Votre commentaire</div>
     <div class = "panel-body">
         <div class = "col-sm-12">
             <form action = "{{ route('comment.store') }}" method = "post" class = "form-horizontal panel">
@@ -8,7 +8,7 @@
                 <input type = "hidden" name = "bug_id" id = "bug_id" value = "{{$bug->id}}">
                 <div class = "{!! $errors->has('title') ? 'has-error' : '' !!}">
                             <textarea class = "form-group" name = "body" id = "body" style = "width:100%" rows = "10"
-                                      placeholder = "Descriptif"></textarea>
+                                      placeholder = "Tapez votre commentaire"></textarea>
                     {!! $errors->first('title', '<small class="help-block">:message</small>') !!}
                 </div>
                 <button type = "submit" class = "btn btn-primary pull-right">Envoyer</button>
