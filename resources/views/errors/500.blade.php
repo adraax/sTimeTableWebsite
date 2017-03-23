c9333f85625ecb4a53af5b2ed0ef7632281520db
Adraax <merlin.olivier71@gmail.com>
2017-03-23 15:49:30 +0100

    update 500 view

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.template')
 
 @section('content')
-    <h1>Oups</h1>
-    <p>Une erreur est survenue. Nous sommes désolé du désagrément occasionné :(</p>
+    <div class = "col-m-offset-1 col-m-10">
+        <h1>Oups</h1>
+        <p>Une erreur est survenue. Nous sommes désolé du désagrément occasionné :(</p>
+    </div>
 @endsection
