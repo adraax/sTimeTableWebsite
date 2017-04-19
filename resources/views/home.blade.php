5a421424b4fd0f49f9477c6be5abf3c372dd2b7a
Olivier Merlin <merlin.olivier71@gmail.com>
2017-04-19 15:22:48 +0200

    delete "domain changed" alert

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,6 @@
 @section('content')
     <div class="alert alert-dismissible alert-info">
         <button type="button" class="close" data-dismiss="alert">&times;</button>
-        L'adresse du site a changé, c'est maintenant <em>www.adraax.eu</em>.
-        <br>
-        Cela n'a aucun impact pour vous, en dehors d'une déconnexion potentielle.
-    </div>
-    <div class="alert alert-dismissible alert-info">
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
         Afin de corriger les bugs le plus efficacement possible, des mails automatiques seront bientôt envoyés par le
         site.
         <br>
