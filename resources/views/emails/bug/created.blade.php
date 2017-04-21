939e4b8e48621324d300cb47cfeb983c6c228d4a
Olivier Merlin <merlin.olivier71@gmail.com>
2017-04-21 10:20:35 +0200

    fix mail templates

--- resources/views/emails/bug/created.blade.php
+++ resources/views/emails/bug/created.blade.php
@@ -1,21 +1,21 @@
 @component('mail::message')
-    # Nouveau Bug !
+# Nouveau Bug !
 
-    [{{$bug->user->name}}]({!! route('user.show', $bug->user->id) !!}) a créé un nouveau bug :
+[{{$bug->user->name}}]({!! route('user.show', $bug->user->id) !!}) a créé un nouveau bug :
 
-    @component('mail::panel')
-        ## {{$bug->title}}
-        {!! nl2br(e($bug->description)) !!}
-    @endcomponent
+@component('mail::panel')
+## {{$bug->title}}
+{!! nl2br(e($bug->description)) !!}
+@endcomponent
 
-    <br>
+<br>
 
-    @component('mail::button', ['url' => route('bug.show', $bug->id)])
-        Aller voir le bug.
-    @endcomponent
+@component('mail::button', ['url' => route('bug.show', $bug->id)])
+Aller voir le bug.
+@endcomponent
 
 
-    Team Pluto
-    <br>
-    <small>Ceci est un mail automatique, merci de ne pas y répondre.</small>
+Team Pluto
+<br>
+<small>Ceci est un mail automatique, merci de ne pas y répondre.</small>
 @endcomponent
--- resources/views/emails/comment/created.blade.php
+++ resources/views/emails/comment/created.blade.php
@@ -1,19 +1,19 @@
 @component('mail::message')
-    # Nouveau commentaire
+# Nouveau commentaire
 
-    [{{$comment->user->name}}]({!! route('user.show', $comment->bug->user->id) !!}) a commenté le bug :
-    _{{$comment->bug->title}}_ auquel vous avez participé.
+[{{$comment->user->name}}]({!! route('user.show', $comment->bug->user->id) !!}) a commenté le bug :
+_{{$comment->bug->title}}_ auquel vous avez participé.
 
-    @component('mail::panel')
-        {!! nl2br(e($comment->content)) !!}
-    @endcomponent
+@component('mail::panel')
+{!! nl2br(e($comment->content)) !!}
+@endcomponent
 
-    @component('mail::button', ['url' => route('bug.show', $comment->bug->id)])
-        Aller voir le bug.
-    @endcomponent
+@component('mail::button', ['url' => route('bug.show', $comment->bug->id)])
+Aller voir le bug.
+@endcomponent
 
 
-    Team Pluto
-    <br>
-    <small>Ceci est un mail automatique, merci de ne pas y répondre.</small>
+Team Pluto
+<br>
+<small>Ceci est un mail automatique, merci de ne pas y répondre.</small>
 @endcomponent
