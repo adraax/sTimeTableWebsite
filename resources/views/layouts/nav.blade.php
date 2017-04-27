bd6ff52864b89f52c45aa330ceac387ec8b4a2c5
Olivier Merlin <olivier@loungeup.com>
2017-04-27 16:05:32 +0200

    store button

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,8 +1,9 @@
 <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+    <a href='https://play.google.com/store/apps/details?id=fr.master_developpement_logiciel.teampluto.stimetable&hl=fr&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img
+                class="store-button" alt='Disponible sur Google Play'
+                src='https://play.google.com/intl/en_us/badges/images/generic/fr_badge_web_generic.png'/></a>
     <div class="sidebar-module sidebar-module-inset">
-        <a href='https://play.google.com/store/apps/details?id=fr.master_developpement_logiciel.teampluto.stimetable&hl=fr&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img
-                    class="store-button" alt='Disponible sur Google Play'
-                    src='https://play.google.com/intl/en_us/badges/images/generic/fr_badge_web_generic.png'/></a>
+
         <h4>À propos</h4>
         <p>
             Gérez votre emploi du temps depuis votre smartphone, ajoutez vos enseignants et notifiez-les d'un
