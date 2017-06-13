1eb7e84d0f39bb28def5089151906bb7e76db33e
Olivier Merlin <olivier@loungeup.com>
2017-06-13 11:06:39 +0200

    update wiki

--- resources/views/wiki/menu/settings/synchro.blade.php
+++ resources/views/wiki/menu/settings/synchro.blade.php
@@ -11,16 +11,16 @@
     <div class="row">
         <div class="col-sm-8 blog-main">
             <div class="blog-post">
-                <h2 class="blog-post-title">Menu - Préférences - Notifications</h2>
+                <h2 class="blog-post-title">Menu - Préférences - Synchronisation</h2>
                 <p class="blog-post-meta">
                     Modifié pour la dernière fois
-                    le {{\Carbon\Carbon::createFromFormat('d-m-Y', '12-06-2017')->formatLocalized('%A %d %B %Y')}}
+                    le {{\Carbon\Carbon::createFromFormat('d-m-Y', '13-06-2017')->formatLocalized('%A %d %B %Y')}}
                 </p>
 
                 <p>
                     Sur cette vue vous pouvez choisir d'activer ou non la synchronisation automatique, c'est-à-dire la mise à jour programmée de votre emploi du temps.
                     <br>
-                    Vous pouvez également choisir la fréquence d'actualisation de votre emploi du temps pour la synchronisation.
+                    Vous pouvez également choisir la fréquence d'actualisation.
                 </p>
 
                 <img src="{{URL::asset('images/settingsSynchro.png')}}" alt="" class="wiki-image">
